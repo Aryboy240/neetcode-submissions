a1f5a959fa4a2c5c23c9f4eb55d3ffc4cc351372
Aryan :) <94476184+Aryboy240@users.noreply.github.com>
2026-04-26 21:24:56 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.9
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Peak"
+print(type(variable))
+variable = ["My goat", "House"]
+print(type(variable))
